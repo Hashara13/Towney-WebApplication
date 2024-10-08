5e69894e75f8815a9fe284d70c89f6fe88884bc8
Hashara13 <hnethmin13@gmail.com>
2024-10-08 21:00:25 +0530

    edited

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -23,3 +23,28 @@ router.get('/create/group', async (req,res)=>{
     res.status(500).json({error:'An error occurred while fetching data'})
    } 
 })
+
+router.get('/create/group/:id', async (req,res)=>{
+    try{
+     const groupId=req.params.id;
+     const crew_Group=await CrewGroup.findById(groupId)
+     res.status(201).json(crew_Group)
+    }catch(err){
+     console.error('Error in Fetching Group Data',err)
+     res.status(500).json({error:'An error occurred while fetching data'})
+    } 
+ })
+
+ router.put('/create/group/edit/:id', async (req,res)=>{
+    try{
+     const groupId=req.params.id;
+     const UpdateCrew_Group=await CrewGroup.findByIdAndUpdate(groupId,req.body,{new:true})
+     if (!UpdateCrew_Group) {
+        return res.status(404).json({ error: 'Group not found' });
+    }
+     res.status(201).json(UpdateCrew_Group)
+    }catch(err){
+     console.error('Error in Fetching Group Data',err)
+     res.status(500).json({error:'An error occurred while fetching data'})
+    } 
+ })
