3142f041ce38f2e1cf774c9a356d12e8d232bf51
Hashara13 <hnethmin13@gmail.com>
2024-10-18 11:40:19 +0530

    profile routes

---
+++ backend/models/myProfile.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const profileSchema = new mongoose.Schema({
+    filename: {
+      type: String,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+    uploadDate: {
+      type: Date,
+      default: Date.now,
+    },
+  });
+
+const MyProfile = mongoose.model("mypProfile", profileSchema); 
+
+module.exports = MyProfile;
---
+++ backend/routes/profileRout.js
@@ -0,0 +1,11 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const multer=require('multer')
+const path=require('path')
+const cors=require('cors')
+const router=express.Router()
+const Profile = require('../models/myProfile');
+
+
+
+
