53c99fee0cad1cf23fa444c1a9052cb01a57cf0e
Hashara13 <hnethmin13@gmail.com>
2024-10-18 11:47:25 +0530

    route

--- backend/routes/profileRout.js
+++ backend/routes/profileRout.js
@@ -6,6 +6,19 @@ const cors=require('cors')
 const router=express.Router()
 const Profile = require('../models/myProfile');
 
+const storage=multer.diskStorage({
+    destination:(req, file, cb)=>{
+        cb(null, 'uploads/')
+    },
+    filename:(req, file, cb)=>{
+        cb(null, Dat.now()+path.extname(file.originalname))
+    }
+})
 
+const upload=multer({storage:storage})
+
+app.post('/create/upload', upload.single('photo'), async(req, res)=>{
+    
+})
 
 
