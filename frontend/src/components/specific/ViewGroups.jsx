2725606a89123a3c262b5f86a0e8c84dd7ac8b9f
Hashara13 <hnethmin13@gmail.com>
2024-10-24 23:45:35 +0530

    workspace

--- frontend/src/components/specific/ViewGroups.jsx
+++ frontend/src/components/specific/ViewGroups.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
-import SideBar from './SideBar'
+import React, { useEffect, useState } from "react";
+import axios from "axios";import SideBar from './SideBar'
 
 const ViewGroups = () => {
+  const [groups, setGroups] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const viewGroups = async () => {
+      try {
+        const response = await axios.get("http://localhost:5000/view/group");
+        setGroups(response.data);
+      } catch (err) {
+        console.error("Error fetching groups:", err);
+        setError("Failed to fetch groups. Please try again later.");
+      }
+    };
+    viewGroups();
+  }, []);
+
+
+
   return (
     <div className="flex">
         <SideBar/>
