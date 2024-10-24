08d66ef4c63dbb291fa984a97e9c78783997bf1f
Hashara13 <hnethmin13@gmail.com>
2024-10-24 22:57:25 +0530

    minor changes

--- frontend/src/pages/FindGroups.jsx
+++ frontend/src/pages/FindGroups.jsx
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 const FindGroups = () => {
+  const [groups, setGroups] = useState([]);
+  const [fileterGroups, setfileterGroups] = useState([]);
+  const [searchText, setSearchTexts] = useState("");
+
+  useEffect(() => {
+    const viewGroups = () => {
+      axios
+        .get("http://localhost:5000//view/group")
+        .then((res) => {
+          setGroups(res.data);
+          setfileterGroups(res.data);
+        })
+        .catch((err) => {
+          console.error("Error fetching producers:", err);
+          alert("Failed to fetch producers. Check console for details.");
+        });
+    };
+    viewGroups()
+  },[]);
+
   return (
     <div>
       <div className="bg-gradient-to-r from-purple-600 to-indigo-600 text-white">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24 text-center">
           <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl">
-          Collaborate with Expert Teams          </h1>
+            Collaborate with Expert Teams{" "}
+          </h1>
           <p className="mt-6 max-w-2xl mx-auto text-2xl">
-          Join  with industry-leading professionals to achieve success.
+            Join with industry-leading professionals to achieve success.
           </p>
           <div className="mt-10 max-w-lg mx-auto">
             <div className="flex items-center bg-white rounded-lg overflow-hidden shadow-md">
